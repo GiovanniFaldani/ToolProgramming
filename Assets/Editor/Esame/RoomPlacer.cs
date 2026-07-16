04c1eef35063f675664c06fb6c2cc47211d82735
Giovanni Faldani <faldanig@yahoo.it>
2026-07-16 13:06:20 +0200

    Consolidamento in unico script, draw della preview

--- Assets/Editor/Esame/RoomPlacer.cs
+++ Assets/Editor/Esame/RoomPlacer.cs
@@ -1,18 +1,43 @@
+using System.Collections.Generic;
+using System.IO;
 using UnityEditor;
 using UnityEngine;
 
 public class RoomPlacer : EditorWindow
 {
+    // classe per memorizzare i dati di una cartella di prefab
+    private class CategoryData
+    {
+        public string name;
+        public bool foldout; // stato del foldout: aperto/chiuso
+        public string[] prefabPaths; // path relativi a Unity
+        public string[] prefabNames;
+        public GameObject[] prefabAssets; // riferimenti ai prefab in memoria
+    }
+
     [MenuItem("Tools/Room Placer")]
     public static void Open()
     {
         GetWindow<RoomPlacer>("Room Placer");
+        ScanFolders();
     }
 
     private bool isPlacementMode = false;
-    public bool rangeVisibility = false;
-    public float snapRange = 20f;
-    public GameObject selectedPrefab = null;
+    private bool radiusVisibility = false;
+    private float snapRadius = 20f;
+    private CategoryData selectedCategory = null;
+    private GameObject selectedPrefab = null;
+    private GameObject previewObject = null;
+
+    private Vector2 scrollPos;
+
+    private static readonly List<CategoryData> categories = new List<CategoryData>();
+    private const string RootFolder = "Assets/Prefab/Rooms";
+
+    private float prefabButtonHeight = 100;
+    private float prefabButtonWidth = 200;
+
+    private int lastSelectIndex = int.MinValue;
 
     private void OnGUI()
     {
@@ -27,6 +52,8 @@ public class RoomPlacer : EditorWindow
             {
                 isPlacementMode = true;
                 SceneView.duringSceneGui += OnSceneGUI;
+
+                ScanFolders();
             }
         }
         // se la placement mode è attiva
@@ -35,17 +62,44 @@ public class RoomPlacer : EditorWindow
             if (GUILayout.Button("Disable Placement Mode"))
             {
                 isPlacementMode = false;
+                lastSelectIndex = int.MinValue;
                 SceneView.duringSceneGui -= OnSceneGUI;
 
                 // distruggo la preview
                 ClearPreview();
             }
 
-            EditorGUILayout.Toggle("View Snap Radius", rangeVisibility);
+            GUILayout.Space(10f);
+
+            if (GUILayout.Button("Refresh")) ScanFolders();
 
             GUILayout.Space(10f);
 
-            EditorGUILayout.FloatField("Snap Radius", snapRange);
+            radiusVisibility = EditorGUILayout.Toggle("View Snap Radius", radiusVisibility);
+
+            GUILayout.Space(10f);
+
+            snapRadius = EditorGUILayout.FloatField("Snap Radius", snapRadius);
+
+            GUILayout.Space(10f);
+
+            // pulsanti di selezione categoria
+            foreach (CategoryData cat in categories)
+            {
+                if (GUILayout.Button(cat.name))
+                {
+                    selectedCategory = cat;
+                    lastSelectIndex = int.MinValue;
+                    SceneView.RepaintAll();
+                }
+            }
+
+            GUILayout.Space(10f);
+
+            if(selectedPrefab != null)
+                EditorGUILayout.LabelField($"Currently Selected Prefab: {selectedPrefab.name}");
+            else
+                EditorGUILayout.LabelField($"Currently Selected Prefab: None");
 
             GUILayout.Space(10f);
         }
@@ -53,15 +107,96 @@ public class RoomPlacer : EditorWindow
 
     void OnSceneGUI(SceneView sceneView)
     {
-        if (!isPlacementMode)
+        Debug.Log("OnSceneGUI tick");
+
+        if (!isPlacementMode || selectedCategory == null)
             return;
 
+        if (categories.Count <= 0)
+        {
+            EditorGUILayout.HelpBox("No prefab found in the specified folder", MessageType.Info);
+            return;
+        }
+
+        Handles.BeginGUI();
+
+        // TODO draw dei bottoni in scena per spawnare prefab
 
         // mi prendo l'evento dell'input
         Event e = Event.current;
 
+        // crea uno scrolling menu di ogni prefab nella categoria corrente
+        scrollPos = GUILayout.BeginScrollView(scrollPos, GUILayout.MaxHeight(1000));
+
+        for(int i = 0; i < selectedCategory.prefabAssets.Length; i++)
+        {
+            GameObject asset = selectedCategory.prefabAssets[i];
+
+            // GetAssetPreview è asincrono
+            Texture2D preview = AssetPreview.GetAssetPreview(asset);
+
+            // GUI CONTENT combina immagine + tooltip in un unico oggetto
+            GUIContent content;
+
+            if (preview != null)
+            {
+                content = new GUIContent(preview, selectedCategory.prefabNames[i]);
+            }
+            else
+            {
+                content = new GUIContent(selectedCategory.prefabNames[i]);
+            }
+
+            GUILayout.Space(10f);
+
+            if (i == lastSelectIndex) GUI.backgroundColor = Color.lightBlue;
+            else GUI.backgroundColor = Color.white;
+
+            // quando clicco sul pulsante del prefab, cambio il selezionato e consumo l'input
+            if (GUILayout.Button(content, GUILayout.Width(prefabButtonWidth), GUILayout.Height(prefabButtonHeight)))
+            {
+                Debug.Log($"Cliccato su: {selectedCategory.prefabNames[i]}");
+                selectedPrefab = asset;
+                lastSelectIndex = i;
+                Repaint();
+                e.Use();
+
+                // TODO istanzia prefab in scena
+                previewObject = (GameObject)PrefabUtility.InstantiatePrefab(selectedPrefab);
+
+                // TODO register this undo when the prefab is PLACED, not spawned
+                Undo.RegisterCreatedObjectUndo(previewObject, "Spawn Room prefab");
+
+            }
+
+            GUI.backgroundColor = Color.white;
+        }
+
+        if (GUILayout.Button("Undo", GUILayout.Width(prefabButtonWidth), GUILayout.Height(prefabButtonHeight)))
+        {
+            // TODO chiama funzione di undo
+        }
+
+        GUILayout.EndScrollView();
+
+        Handles.EndGUI();
+
+        // TODO se preview è istanziata, seguire la posizione del mouse, display raggio snap e funzione di snap
+        if (previewObject == null) return;
+
         // raggio che parte dal mouse
         Ray ray = HandleUtility.GUIPointToWorldRay(e.mousePosition);
+
+        Plane roomLevel = new Plane(Vector3.up, new Vector3(0,1,0));
+
+        if (!roomLevel.Raycast(ray, out float dist)) return;
+
+        Vector3 position = Camera.main.transform.position + (ray.direction.normalized * dist);
+
+        previewObject.transform.position = position;
+
+
+
 
         // refresh della scena
         sceneView.Repaint();
@@ -74,11 +209,66 @@ public class RoomPlacer : EditorWindow
         ClearPreview();
     }
 
-    void ClearPreview()
+    private static void ScanFolders()
+    {
+        categories.Clear();
+
+        if (!AssetDatabase.IsValidFolder(RootFolder)) return;
+
+        // controlla tutte le sottocartelle del root folder assegnato
+        string fullRootPath = Path.GetFullPath(RootFolder);
+        string[] subDirs = Directory.GetDirectories(fullRootPath); // 1 door rooms, 2 door rooms...
+
+        foreach (string dir in subDirs)
+        {
+            string folderName = Path.GetFileName(dir);
+
+            // ricostruire il path relativo di Unity
+            string assetFolderPath = RootFolder + "/" + folderName;
+
+            // trova tutti i prefab nelle subdirectory
+            string[] guids = AssetDatabase.FindAssets("t:prefab", new[] { assetFolderPath });
+
+            // cartella vuota skip
+            if (guids.Length == 0) continue;
+
+            // crea un nuovo categoryData per la cartella corrente
+            var category = new CategoryData()
+            {
+                name = folderName,
+                foldout = false,
+                prefabPaths = new string[guids.Length],
+                prefabNames = new string[guids.Length],
+                prefabAssets = new GameObject[guids.Length],
+            };
+
+            // per ogni prefab nella cartella
+            for (int i = 0; i < guids.Length; i++)
+            {
+                // conversione GUID --> PATH LEGGIBILE
+                category.prefabPaths[i] = AssetDatabase.GUIDToAssetPath(guids[i]);
+
+                category.prefabNames[i] = Path.GetFileNameWithoutExtension(category.prefabPaths[i]);
+
+                // load asset path carica il riferimento in memoria (NON istanzia)
+                category.prefabAssets[i] = AssetDatabase.LoadAssetAtPath<GameObject>(category.prefabPaths[i]);
+            }
+
+            // aggiunge alla lista dei prefab disponibili da GUI
+            categories.Add(category);
+        }
+
+        Debug.Log($"Categories found: {categories.Count}");
+    }
+
+    private void ClearPreview()
     {
         // distruggiamo la preview se esiste
-        if (selectedPrefab != null)
-            DestroyImmediate(selectedPrefab);
+        if (previewObject != null)
+        {
+            DestroyImmediate(previewObject);
+            previewObject = null;
+        }
     }
 
 
--- Assets/Editor/Esame/RoomPlacerGUI.cs
+++ Assets/Editor/Esame/RoomPlacerGUI.cs
@@ -1,61 +1,196 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using UnityEditor;
-using UnityEditor.Overlays;
-using UnityEngine;
+//using System;
+//using System.Collections.Generic;
+//using System.IO;
+//using UnityEditor;
+//using UnityEditor.Overlays;
+//using UnityEngine;
 
-[Overlay(typeof(SceneView), "Room Placer")]
-public class RoomPlacerGUI : IMGUIOverlay
-{
-    private const string RootFolder = "Assets/Prefab";
-    // TODO trova come spacchio pescare una reference alla window, o fare drawing su sceneGUI dall'altro script
-    // private static readonly RoomPlacer roomPlacerWindow = EditorWindow.GetWindow<RoomPlacer>();
+//[Overlay(typeof(SceneView), "Room Placer")]
+//public class RoomPlacerGUI : IMGUIOverlay
+//{
+//    // classe per memorizzare i dati di una cartella di prefab
+//    private class CategoryData
+//    {
+//        public string name;
+//        public bool foldout; // stato del foldout: aperto/chiuso
+//        public string[] prefabPaths; // path relativi a Unity
+//        public string[] prefabNames;
+//        public GameObject[] prefabAssets; // riferimenti ai prefab in memoria
+//    }
 
-    public override void OnCreated()
-    {
-        ScanFolders();
+//    private Vector2 scrollPos;
 
-        SceneView.duringSceneGui -= OnScenePreview;
-        SceneView.duringSceneGui += OnScenePreview;
+//    private static readonly List<CategoryData> categories = new List<CategoryData>();
+//    private const string RootFolder = "Assets/Prefab/Rooms";
 
-        displayedChanged -= OnVisibilityChanged;
-        displayedChanged += OnVisibilityChanged;
-    }
+//    private int columns = 3;
+//    private float thumbsize = 64;
 
-    private void OnVisibilityChanged(bool obj)
-    {
-        if (!obj)
-        {
-            ResetSelection();
-        }
-    }
-    public override void OnWillBeDestroyed()
-    {
-        SceneView.duringSceneGui -= OnScenePreview;
-        displayedChanged -= OnVisibilityChanged;
+//    private static GameObject selectedPrefab;
+//    private static string selectedName;
+//    private static Mesh previewMesh;
+//    private static Material[] previewMaterials;
+//    private static Vector3 previewPosition;
+//    private static Vector3 previewScale;
 
-        ResetSelection();
-    }
+//    public override void OnCreated()
+//    {
+//        ScanFolders();
 
-    private static void ScanFolders()
-    {
+//        SceneView.duringSceneGui -= OnScenePreview;
+//        SceneView.duringSceneGui += OnScenePreview;
 
-    }
+//        displayedChanged -= OnVisibilityChanged;
+//        displayedChanged += OnVisibilityChanged;
+//    }
 
-    public override void OnGUI()
-    {
-        throw new NotImplementedException();
-    }
+//    private void OnVisibilityChanged(bool obj)
+//    {
+//        if (!obj)
+//        {
+//            ResetSelection();
+//        }
+//    }
+//    public override void OnWillBeDestroyed()
+//    {
+//        SceneView.duringSceneGui -= OnScenePreview;
+//        displayedChanged -= OnVisibilityChanged;
 
-    private static void OnScenePreview(SceneView sceneView)
-    {
+//        ResetSelection();
+//    }
 
-    }
+//    private static void ScanFolders()
+//    {
+//        categories.Clear();
 
-    private static void ResetSelection()
-    {
-        // roomPlacerWindow.selectedPrefab = null;
-    }
+//        if (!AssetDatabase.IsValidFolder(RootFolder)) return;
 
-}
+//        // controlla tutte le sottocartelle del root folder assegnato
+//        string fullRootPath = Path.GetFullPath(RootFolder);
+//        string[] subDirs = Directory.GetDirectories(fullRootPath); // 1 door rooms, 2 door rooms...
+
+//        foreach (string dir in subDirs)
+//        {
+//            string folderName = Path.GetFileName(dir);
+
+//            // ricostruire il path relativo di Unity
+//            string assetFolderPath = RootFolder + "/" + folderName;
+
+//            // trova tutti i prefab nelle subdirectory
+//            string[] guids = AssetDatabase.FindAssets("t:prefab", new[] { assetFolderPath });
+
+//            // cartella vuota skip
+//            if (guids.Length == 0) continue;
+
+//            // crea un nuovo categoryData per la cartella corrente
+//            var category = new CategoryData()
+//            {
+//                name = folderName,
+//                foldout = false,
+//                prefabPaths = new string[guids.Length],
+//                prefabNames = new string[guids.Length],
+//                prefabAssets = new GameObject[guids.Length],
+//            };
+
+//            // per ogni prefab nella cartella
+//            for (int i = 0; i < guids.Length; i++)
+//            {
+//                // conversione GUID --> PATH LEGGIBILE
+//                category.prefabPaths[i] = AssetDatabase.GUIDToAssetPath(guids[i]);
+
+//                category.prefabNames[i] = Path.GetFileNameWithoutExtension(category.prefabPaths[i]);
+
+//                // load asset path carica il riferimento in memoria (NON istanzia)
+//                category.prefabAssets[i] = AssetDatabase.LoadAssetAtPath<GameObject>(category.prefabPaths[i]);
+//            }
+
+//            // aggiunge alla lista dei prefab disponibili da GUI
+//            categories.Add(category);
+//        }
+//    }
+
+//    public override void OnGUI()
+//    {
+//        if (GUILayout.Button("Refresh")) ScanFolders();
+
+//        if (categories.Count <= 0)
+//        {
+//            EditorGUILayout.HelpBox("No prefab found in the specified folder", MessageType.Info);
+//            return;
+//        }
+
+//        // scrollPos tiene traccia della posizione di scroll tra un frame e l'altro
+//        scrollPos = GUILayout.BeginScrollView(scrollPos, GUILayout.MaxHeight(300));
+
+//        foreach (CategoryData cat in categories)
+//        {
+//            // crea foldout della categoria di stanza corrispondente
+//            cat.foldout = EditorGUILayout.Foldout(cat.foldout, $"{cat.name} ({cat.prefabPaths.Length})", true);
+
+//            if (cat.foldout)
+//            {
+//                // conta il numero di righe da mettere nel foldout
+//                int itemCount = cat.prefabPaths.Length;
+
+//                int rows = Mathf.CeilToInt((float)itemCount / columns);
+
+//                // display della categoria in righe
+//                for (int row = 0; row < rows; row++)
+//                {
+//                    using (new GUILayout.HorizontalScope())
+//                    {
+//                        for (int col = 0; col < columns; col++)
+//                        {
+//                            int index = row * columns + col;
+
+//                            if (index >= itemCount)
+//                            {
+//                                // cella vuota
+//                                GUILayout.Label("", GUILayout.Width(thumbsize), GUILayout.Height(thumbsize));
+//                                continue;
+//                            }
+
+//                            GameObject prefab = cat.prefabAssets[index];
+
+//                            // GetAssetPreview è asincrono
+//                            Texture2D preview = AssetPreview.GetAssetPreview(prefab);
+
+//                            // GUI CONTENT combina immagine + tooltip in un unico oggetto
+//                            GUIContent content;
+
+//                            if (preview != null)
+//                            {
+//                                content = new GUIContent(preview, cat.prefabNames[index]);
+//                            }
+//                            else
+//                            {
+//                                content = new GUIContent(cat.prefabNames[index]);
+//                            }
+
+//                            if (GUILayout.Button(content, GUILayout.Width(thumbsize), GUILayout.Height(thumbsize)))
+//                            {
+//                                Debug.Log($"Cliccato su: {cat.prefabNames[index]}");
+//                                //RoomPlacer.Instance.selectedPrefab = prefab;
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//        }
+
+//        GUILayout.EndScrollView();
+//    }
+
+//    private static void OnScenePreview(SceneView sceneView)
+//    {
+
+//    }
+
+
+//    private static void ResetSelection()
+//    {
+//        // resetta alla prima stanza con una porta
+//        // RoomPlacer.Instance.selectedPrefab = categories[0].prefabAssets[0];
+//    }
+
+//}
